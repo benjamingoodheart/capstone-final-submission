8b3cf163f9b692113ac513c7d82cf3ae7fd18b16
bengoodheart <bgoodheart@fordham.edu>
2022-05-09 18:55:20 -0400

    BUG: Added CDN

--- aoty_helper/settings.py
+++ aoty_helper/settings.py
@@ -146,12 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'capstone-final-submission/static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'capstone-final-submission/static/')
 STATICFILES_DIRS = [
-    'capstone-final-submission/static',
+    'static/templates',
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -181,13 +180,14 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Cookies
-
+'''
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SESSION_SAVE_EVERY_REQUEST = True
 
 SECURE_SSL_REDIRECT = False
+'''
 
 
 # EMAIL SETTINGS
